e8ffdbbdbad12254684ee7cfa1cf0781aeddca15
Victor Miguez <dev.miguezz@gmail.com>
2024-02-19 17:20:28 -0300

    Trabalhando com Arrays

---
+++ arrays/index.js
@@ -0,0 +1,65 @@
+const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli"]
+
+// adicionar elementos
+// push: Adiciona um elemento no final do array e devolve o novo tamanho do array
+let tamanho = arr.push("Boromir")
+console.log(arr)
+console.log(tamanho)
+
+// unshift: Adiciona um elemento no começo do array e devolve o novo tamanho do array
+tamanho = arr.unshift("Boromir")
+console.log(arr)
+console.log(tamanho)
+
+
+// remover elementos
+// pop: Remove um elemento no final do array e devolve o elemento removido
+let elementoRemovido = arr.pop("Boromir")
+console.log(arr)
+console.log(elementoRemovido)
+
+// shift: Remove um elemento no começo do array e devolve o elemento removido
+elementoRemovido = arr.shift("Boromir")
+console.log(arr)
+console.log(elementoRemovido)
+
+
+// pesquisar elementos
+// includes: Verifica se um certo elemento está presente no array 
+const inclui = arr.includes("Gandalf")
+console.log(inclui)
+
+// indexOf: Encontra o índice que possui o elemento indicado, ou -1 caso ele não exista no array
+const indice = arr.indexOf("Gandalf")
+console.log(indice)
+
+
+// cortar e concatenar
+// slice: Copia uma fatia do array e a retorna sem alterar o array original
+const hobbits = arr.slice(0, 4)
+// Também pode ser usado com números negativos. neste exemplo, pegamos os elementos exceto os 4 primeiros.
+const outros = arr.slice(-4)
+console.log(arr)
+console.log(hobbits)
+console.log(outros)
+
+// concat: Junta dois ou mais arrays e/ou itens e devolve o resultado (sem alterar nenhum dos arrays)
+const sociedade = hobbits.concat(outros, "Boromir")
+console.log(sociedade)
+console.log(hobbits)
+console.log(outros)
+
+
+// substituir elementos
+// splice: Permite remover elementos em qualquer posição do array e substituir por novos
+const elementosRemovidos = sociedade.splice(indice, 1, "Gandalf, o Cinzento")
+console.log(elementosRemovidos)
+console.log(sociedade)
+
+
+// iterar sobre elementos
+// Usando o for para percorrer cada elemento do array
+for (let indice = 0; indice < sociedade.length; indice++) {
+    const elemento = sociedade[indice]
+    console.log(elemento + " se encontra na posição " + indice)
+}
